01ed00d7855c8ab9ea69148dcd7d68a1ecf4d1e7
Nina Lutz <nlutz@mit.edu>
2016-09-17 15:54:33 -0400

    updates and javascript animation

--- sketch.js
+++ sketch.js
@@ -1,144 +1,65 @@
-// Demonstration of multiple force acting on
-// bodies (Mover class)
-// Bodies experience gravity continuously
-// Bodies experience fluid resistance when in "water"
+var NUMSINES = 20; // how many of these things can we do at once?
+var sines = new Array(NUMSINES); // an array to hold all the current angles
+var rad; // an initial radius value for the central sine
+var i; // a counter variable
 
-// Five moving bodies
-var movers = [];
+// play with these to get a sense of what's going on:
+var fund = 0.005; // the speed of the central sine
+var ratio = 1; // what multiplier for speed is each additional sine?
+var alpha = 50; // how opaque is the tracing system
 
-// Liquid
-var liquid;
+var trace = false; // are we tracing?
 
 function setup() {
-  createCanvas(425, 300);
-  reset();
-  // Create liquid object
-  liquid = new Liquid(0, height/2, width, height/2, 0.1);
+  createCanvas(300, 200);
+
+  rad = height/4; // compute radius for central circle
+  background(255); // clear the screen
+
+  for (var i = 0; i<sines.length; i++) {
+    sines[i] = PI; // start EVERYBODY facing NORTH
+  }
 }
 
 function draw() {
-  // test();
-  background(255);
+  if (!trace) {
+    background(255); // clear screen if showing geometry
+    stroke(88, 255); // black pen
+    noFill(); // don't fill
+  }
 
-  // Draw water
-  liquid.display();
+  // MAIN ACTION
+  push(); // start a transformation matrix
+  translate(width/2, height/2); // move to middle of screen
 
-  for (var i = 0; i < movers.length; i++) {
-
-    // Is the Mover in the liquid?
-    if (liquid.contains(movers[i])) {
-      // Calculate drag force
-      var dragForce = liquid.calculateDrag(movers[i]);
-      // Apply drag force to Mover
-      movers[i].applyForce(dragForce);
+  for (var i = 0; i<sines.length; i++) {
+    var erad = 0; // radius for small "point" within circle... this is the 'pen' when tracing
+    // setup for tracing
+    if (trace) {
+      stroke(0, 0, 255*(float(i)/sines.length), alpha); // blue
+      fill(0, 0, 255, alpha/2); // also, um, blue
+      erad = 5.0*(1.0-float(i)/sines.length); // pen width will be related to which sine
     }
-
-    // Gravity is scaled by mass here!
-    var gravity = createVector(0, 0.1*movers[i].mass);
-    // Apply gravity
-    movers[i].applyForce(gravity);
-
-    // Update and display
-    movers[i].update();
-    movers[i].display();
-    movers[i].checkEdges();
+    var radius = rad/(i+1); // radius for circle itself
+    rotate(sines[i]); // rotate circle
+    if (!trace) ellipse(0, 0, radius*2, radius*2); // if we're simulating, draw the sine
+    push(); // go up one level
+    stroke(255, 0, 0, 255);
+    translate(0, radius); // move to sine edge
+    if (!trace) ellipse(0, 0, 5, 5); // draw a little circle
+    if (trace) ellipse(0, 0, erad, erad); // draw with erad if tracing
+    pop(); // go down one level
+    translate(0, radius); // move into position for next sine
+    sines[i] = (sines[i]+(fund+(fund*i*ratio)))%TWO_PI; // update angle based on fundamental
   }
 
+  pop(); // pop down final transformation
+
 }
 
-
-// function test(){
-//   var s = millis();
-//
-//   if(int(s) % 50 === 0){
-//    reset();
-//   }
-// }
-
-function mousePressed() {
-  reset();
-}
-
-// Restart all the Mover objects randomly
-function reset() {
-  for (var i = 0; i < 5; i++) {
-    movers[i] = new Mover(random(0.5, 3), 40+i*70, 0);
+function keyReleased() {
+  if (key==' ') {
+    trace = !trace;
+    background(255);
   }
 }
-
-var Liquid = function(x, y, w, h, c) {
-  this.x = x;
-  this.y = y;
-  this.w = w;
-  this.h = h;
-  this.c = c;
-};
-
-// Is the Mover in the Liquid?
-Liquid.prototype.contains = function(m) {
-  var l = m.position;
-  return l.x > this.x && l.x < this.x + this.w &&
-         l.y > this.y && l.y < this.y + this.h;
-};
-
-// Calculate drag force
-Liquid.prototype.calculateDrag = function(m) {
-  // Magnitude is coefficient * speed squared
-  var speed = m.velocity.mag();
-  var dragMagnitude = this.c * speed * speed;
-
-  // Direction is inverse of velocity
-  var dragForce = m.velocity.copy();
-  dragForce.mult(-1);
-
-  // Scale according to magnitude
-  // dragForce.setMag(dragMagnitude);
-  dragForce.normalize();
-  dragForce.mult(dragMagnitude);
-  return dragForce;
-};
-
-Liquid.prototype.display = function() {
-  noStroke();
-  fill(246, 246, 246);
-  rect(this.x, this.y, this.w, this.h);
-};
-
-function Mover(m,x,y) {
-  this.mass = m;
-  this.position = createVector(x,y);
-  this.velocity = createVector(0,0);
-  this.acceleration = createVector(0,0);
-}
-
-// Newton's 2nd law: F = M * A
-// or A = F / M
-Mover.prototype.applyForce = function(force) {
-  var f = p5.Vector.div(force,this.mass);
-  this.acceleration.add(f);
-};
-
-Mover.prototype.update = function() {
-  // Velocity changes according to acceleration
-  this.velocity.add(this.acceleration);
-  // position changes by velocity
-  this.position.add(this.velocity);
-  // We must clear acceleration each frame
-  this.acceleration.mult(0);
-};
-
-Mover.prototype.display = function() {
-  stroke(0);
-  strokeWeight(2);
-  fill(255, 0, 0);
-  ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
-};
-
-// Bounce off bottom of window
-Mover.prototype.checkEdges = function() {
-  if (this.position.y > (height - this.mass*8)) {
-    // A little dampening when hitting the bottom
-    this.velocity.y *= -0.9;
-    this.position.y = (height - this.mass*8);
-  }
-};
