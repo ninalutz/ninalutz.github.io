0ceda64c835f096d040e9191c0d40a30745f9f32
Nina Lutz <nlutz@mit.edu>
2020-01-19 10:14:09 -0500

    adding 1/19

---
+++ 1_19/sketch.js
@@ -0,0 +1,31 @@
+
+//forked from https://editor.p5js.org/wvnl/sketches 
+
+function setup() {
+  createCanvas(600, 600);
+}
+var timer = 0;
+
+function draw() {
+  timer +=1;
+  background(0);
+  for (let i = 0; i < 600; i += 20) {
+    bezier(
+      i - timer / 2.0,
+      40 + i,
+      410,
+      20,
+      440,
+      300,
+      240 - i / 16.0,
+      300 + i / 8.0
+    );
+  if(timer == 1000){
+    timer = 0;
+  }
+  fill(255);
+  text("1.19.2020", 20, height - 20); 
+}
+}
+
+
