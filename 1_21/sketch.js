05f6ac401b607d42c0903d475c37b0f25163ad71
Nina Lutz <nlutz@mit.edu>
2020-01-21 10:38:20 -0500

    Adding 1/21/20

---
+++ 1_21/sketch.js
@@ -0,0 +1,37 @@
+
+let dim;
+
+function setup() {
+  createCanvas(710, 400);
+  dim = width / 2;
+  background(0);
+  colorMode(HSB, 360, 100, 100);
+  noStroke();
+  ellipseMode(RADIUS);
+  frameRate(1);
+  background(0);
+  fill(255);
+  text("1.21.2019", 20, height - 20); 
+  drawGradient(width/2, height / 2);
+}
+
+function draw() {
+  background(0);
+  drawGradient(width/2, height / 2);
+  drawGradient(width/2 - dim/2, height / 2);
+  drawGradient(width/2 - dim/2, height / 2 - dim/2);
+  drawGradient(width/2, height / 2 - dim/2);
+  fill(255);
+  text("1.21.2020", 20, height - 20); 
+}
+
+function drawGradient(x, y) {
+  let radius = dim / 2;
+  let h = random(0, 360);
+  for (let r = radius; r > 0; --r) {
+    fill(h, 90, 90);
+    rect(x, y, r, r);
+    h = (h + 1) % 360;
+  }
+}
+
