e9bc5199cc400cda10b1ff247d6f2b3cf35fb016
Nina Lutz <nlutz@mit.edu>
2020-02-06 10:36:48 -0500

    adding 2/6

---
+++ 2_6/sketch.js
@@ -0,0 +1,62 @@
+//Based off the particle system example on p5.js
+
+let spin;
+
+let spins = [];
+
+function setup() {
+  createCanvas(600, 600);
+   colorMode(HSB);
+  // Initialize all values
+  for (let i = 0; i < 200; i++) {
+    var dir = -1;
+    if(i%2 == 0){
+      dir = 1;
+    }
+    spin = new Spinner(createVector(width/2, height/2), random(0, 450), dir, 0, random(0.01, 0.1), random(0, 360));
+    spins.push(spin);
+  }
+
+ 
+}
+
+function draw() {
+  // Convert polar to cartesian
+  for (let i = 0; i < spins.length; i++) {
+    spins[i].run();
+  }
+  fill(255);
+  text("2.6.2020", 10, height - 20);
+}
+
+class Spinner{
+ constructor(center, radius, direction, theta, vel, hue) {
+    this.x = 0;
+    this.y = 0; 
+    this.center = center;
+    this.radius = radius;
+    this.direction = direction;
+    this.theta = theta;
+    this.vel = vel;
+    this.hue = hue;
+    this.color = color(hue, 200, 200, 0.3);
+    this.size = random(5, 80);
+  }
+  update(){
+    this.x= (this.radius * cos(this.theta) + this.center.x);
+    this.y = (this.radius * sin(this.theta) + this.center.y);
+    this.theta += this.vel;
+    // this.size += cos(this.theta)*2;
+  }
+  display(){
+    fill(this.color);
+    noStroke();
+    rect(this.x, this.y, this.size, this.size);
+    this.size += 1;
+    if(this.size >= 150) this.size = 5;
+  }
+  run(){
+    this.update();
+    this.display();
+  }
+};
