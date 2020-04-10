a096fb046a5bb2b0c53fbe963a1b6a0e8f664cf8
Nina Lutz <nlutz@mit.edu>
2020-04-10 10:13:37 -0400

    adding sketch

---
+++ Projects/Sketches/4_10/sketch.js
@@ -0,0 +1,65 @@
+function setup() {
+  createCanvas(500, 500);
+}
+var N = 10;
+var r = 60;
+var k=0, t ;
+
+function draw() {
+  background(26);
+  fill(255);
+  noStroke();
+  text("4.10.20", 30, height - 30);
+
+  strokeWeight(5);
+  stroke(255);
+  translate(width>>1, height>>1);
+  for (var i=0; i<N; i++) {
+    var ang = 360/N;
+    var l = r*sin(radians(ang/2));
+    var x = r*cos(radians(ang*i));
+    var y = r*sin(radians(ang*i));
+    push()
+    translate(x, y);
+    rotate(PI/2+radians(ang*i+k));
+    line(- l, 0, l, 0);
+    pop();
+
+    l = r/2*sin(radians(ang/2));
+    x = r/2*cos(radians(ang*i));
+    y = r/2*sin(radians(ang*i));
+
+    push();
+    translate(x, y);
+    rotate(PI/2+radians(-ang*i+k));
+    line(l, 0, -l, 0);
+    pop();
+
+
+    l = r*2*sin(radians(ang/2));
+    x = r*2*cos(radians(ang*i));
+    y = r*2*sin(radians(ang*i));
+
+    push();
+    translate(x, y);
+    rotate(PI/2+radians(-ang*i+k));
+    line(0, -l, 0, l);
+    pop();
+
+
+
+    l = r*3*sin(radians(ang/2));
+    x = r*3*cos(radians(ang*i));
+    y = r*3*sin(radians(ang*i));
+    push()
+    translate(x, y);
+    rotate(PI/2+radians(ang*i+k));
+    line(- l, 0, l, 0);
+    pop();
+
+  }
+  k=180*sin(radians(t));
+  if (t<90) t+=2;
+  else t=0;
+
+}
